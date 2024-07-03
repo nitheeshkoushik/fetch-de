3467df0478d70b0877f761cb490114490dec51f8
nitheeshkoushik <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-07-03 07:32:46 -0400

    etl.py gone

--- etl/etl.py
+++
@@ -1,9 +0,0 @@
-import boto3 
-
-sqs = boto3.client('sqs', endpoint_url="http://localhost:4566")
-
-response = sqs.receive_message(
-            QueueUrl="http://localhost:4566/000000000000/login-queue"
-        )
-
-print(response)
