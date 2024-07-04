7e71814c7614098e117dbc0f2b6462024e609861
nitheeshkoushik <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-07-03 22:27:54 -0400

    using hasing instead of encryption

--- etl/getMessages.py
+++ etl/getMessages.py
@@ -3,13 +3,12 @@ from datetime import datetime
 import json
 from botocore.exceptions import ClientError
 import configparser
+import hashlib
 from encrypt import Encryptor
 
 class SQSReceiver:
 
     def __init__(self, config_file):
-
-        self.encryptor = Encryptor(config_file)
 
         config = configparser.ConfigParser()
         config.read(config_file)
@@ -18,13 +17,18 @@ class SQSReceiver:
         self.queue_url = config.get('AWS', 'queue_url')
         self.sqs = boto3.client('sqs', endpoint_url=self.endpoint_url)
         
+    def hashVal(self, string):
+        sha256 = hashlib.sha256()
+        sha256.update(string.encode('utf-8'))
+        return sha256.hexdigest()
     
     def recieveMessages(self):
         messagesToDelete = []
         batchMessages = []
         try:
             response = self.sqs.receive_message(QueueUrl = self.queue_url, 
-                                        MaxNumberOfMessages=10)
+                                        MaxNumberOfMessages=10, 
+                                         WaitTimeSeconds=1)
             
             messages = response['Messages']
 
@@ -32,20 +36,24 @@ class SQSReceiver:
             date = datetime.strptime(dateStr, '%a, %d %b %Y %H:%M:%S %Z')
 
             for message in messages:
-                    messageNew = json.loads(message['Body'])
-                    messageNew['date'] = date
-                    messageNew['masked_device_id'] = self.encryptor.encrypt(messageNew['device_id'])
-                    del messageNew['device_id']
-                    messageNew['masked_ip'] = self.encryptor.encrypt(messageNew['ip'])
+                messageNew = json.loads(message['Body'])
+                messageNew['date'] = date
+                if 'ip' in messageNew:
+                    messageNew["masked_ip"] = self.hashVal(messageNew['ip'])
                     del messageNew['ip']
+                if 'device_id' in messageNew:
+                    messageNew["masked_device_id"] = self.hashVal(messageNew['device_id'])
+                    del messageNew['device_id']
 
-                    batchMessages.append(messageNew)
-                    messagesToDelete.append(message['ReceiptHandle'])
+                batchMessages.append(messageNew)
+                messagesToDelete.append(message['ReceiptHandle'])
 
             return batchMessages, messagesToDelete
         
         except (ClientError, KeyError) as e:
-             return None, None
+             return [], []
+        
+
     
     def deleteMessages(self, toBeDeletedList):
         for elem in toBeDeletedList:
@@ -55,21 +63,21 @@ class SQSReceiver:
             )
         return None
     
-def main():
-    config_file = '.conf'
-    sqs_manager = SQSReceiver(config_file)
+    def run(self):
+        allMessages = []
 
-    allMessages = []
-    while True:
-        batchMessages, messagesToDelete = sqs_manager.recieveMessages()
-        if messagesToDelete:
-            sqs_manager.deleteMessages(messagesToDelete)
-            allMessages.extend(batchMessages)
-        else:
-             break
-    return allMessages
+        while True:
+            batchMessages, messagesToDelete = self.recieveMessages()
+            if messagesToDelete:
+                self.deleteMessages(messagesToDelete)
+                allMessages.extend(batchMessages)
+            else:
+                break
+        return allMessages
 
 
 if __name__ == "__main__":
-    allMessages = main()
-    print(allMessages[0])
+    config_file = '.conf'
+    sqs = SQSReceiver(config_file)
+    allMessages = sqs.run()
+    print(allMessages, len(allMessages))
