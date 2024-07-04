21cf22bcc4206cf51fc310fff2fb39a74832ecfa
nitheeshkoushik <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-07-03 23:04:32 -0400

    created basic postgres create tables

---
+++ utils/createLoginTable.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+def connect():
+    conn = psycopg2.connect(database = "postgres", user = "postgres", password = "postgres", host = "localhost", port = "5432")
+    return conn
+
+def createTable(cur):
+    cur.execute("""
+            CREATE TABLE IF NOT EXISTS user_logins(
+                user_id varchar(128),
+                device_type varchar(32),
+                masked_ip varchar(256),
+                masked_device_id varchar(256),
+                locale  varchar(32),
+                app_version  varchar(32),
+                create_date date
+                )
+        """)
+    
+    return None
+
+conn = connect()
+cur = conn.cursor()
+createTable(cur)
+conn.commit()
+conn.close()
