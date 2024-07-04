ffe0ac52c44836f339de0a459d0ed8267d943fe4
nitheeshkoushik <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-07-04 09:12:14 -0400

    runner added

---
+++ etl/main.py
@@ -0,0 +1,21 @@
+from createLoginTable import TableCreator
+from getMessages import SQSReceiver
+from insertPostgres import postgresInserter
+
+def main():
+    config_file = '.conf'
+
+    table_creator = TableCreator(config_file)
+    table_creator.createTable()
+
+    sqs = SQSReceiver(config_file)
+    allMessages = sqs.run()
+
+    table_inserter = postgresInserter(allMessages, config_file)  
+    table_inserter.insert()
+
+if __name__ == "__main__":
+    main()
+
+
+
